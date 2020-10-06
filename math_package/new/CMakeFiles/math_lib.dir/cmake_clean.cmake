d5bc5d8a6c0c626d7f6976056eec4d7cbba1d1b6
Adrian Podsiadlowski <adrian.9254@gmail.com>
2020-10-06 19:46:52 +0200

    update

---
+++ math_package/new/CMakeFiles/math_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/math_lib.dir/main.cpp.o"
+  "CMakeFiles/math_lib.dir/math_lib.cpp.o"
+  "bin/math_lib"
+  "bin/math_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/math_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ math_package/new/src/math_lib.h
@@ -0,0 +1,8 @@
+
+int plus(int a, int b);
+
+int minus(int a, int b);
+
+int razy(int a, int b);
+
+int przez(int a, int b);
